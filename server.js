fcd2f314ac56472157ac2d719832de4a216f1fab
Alina Shokota <alina.shokota159@gmail.com>
2020-07-26 20:19:50 +0200

    change server.js file

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/pdf-app'));
+app.use(express.static(__dirname + '/dist/temperature-front'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/pdf-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/temperature-front/index.html'));
 });
 
 // default Heroku PORT
